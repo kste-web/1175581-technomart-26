e1bef52a9523b330556169787f4b4f8dabd60cdf
kste-web <54847495+kste-web@users.noreply.github.com>
2019-10-24 00:55:26 +0300

    Защита 1
    1-ая попытка

--- js/modal-letter.js
+++ js/modal-letter.js
@@ -1,8 +1,10 @@
 var contacts_button = document.querySelector(".contacts-button");
 var modal_letter = document.querySelector(".modal-letter");
+var user_name = modal_letter.querySelector(".modal-letter-name input[name = user-name]");
 
 contacts_button.addEventListener("click", function(evt) {
 	evt.preventDefault();
+	modal_letter.classList.add("modal-letter-show");
 	modal_letter.classList.remove("modal-letter-close");
 	user_name.focus();
 });
@@ -12,5 +14,5 @@ var close_letter = document.querySelector(".modal-letter .close-window");
 close_letter.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	modal_letter.classList.add("modal-letter-close");
-	modal_letter.classList.remove("modal-error");
+	modal_letter.classList.remove("modal-letter-show");
 });
