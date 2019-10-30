b5481e95b6721e43b18f8b60f5405b66ac6e6a31
kste-web <54847495+kste-web@users.noreply.github.com>
2019-10-31 01:23:43 +0300

    На 2-ую итерацию защиты
    исправил ошибки по критериям

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
+var map_small = document.querySelector(".map-small");
+var modal_map = document.querySelector(".modal-map");
+var close_map = document.querySelector(".modal-map .close-window");
 var contacts_button = document.querySelector(".contacts-button");
 var modal_letter = document.querySelector(".modal-letter");
 var user_name = modal_letter.querySelector(".modal-letter-name input[name = user-name]");
+var close_letter = document.querySelector(".modal-letter .close-window");
+
+map_small.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	modal_map.classList.add("modal-map-show");
+	modal_map.classList.remove("modal-map-close");
+});
+
+close_map.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	modal_map.classList.add("modal-map-close");
+	modal_map.classList.remove("modal-map-show");
+});
 
 contacts_button.addEventListener("click", function(evt) {
 	evt.preventDefault();
@@ -8,8 +24,6 @@ contacts_button.addEventListener("click", function(evt) {
 	modal_letter.classList.remove("modal-letter-close");
 	user_name.focus();
 });
-
-var close_letter = document.querySelector(".modal-letter .close-window");
 
 close_letter.addEventListener("click", function(evt) {
 	evt.preventDefault();
